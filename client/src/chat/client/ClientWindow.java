950498f5761dae65e34d5959b0cc528685bf1d90
BadTrap <iudut@bk.ru>
2018-09-20 12:00:11 +0300

    Client side + Windows for client side

--- client/src/chat/client/ClientWindow.java
+++ client/src/chat/client/ClientWindow.java
@@ -1,7 +1,40 @@
 package chat.client;
 
-public class ClientWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class ClientWindow extends JFrame {
+    private static final String IP_ADRR = "192.168.10.97";
+    private static final int PORT = 8189;
+    private static final int HEIGHT = 400;
+    private static final int WIDTH = 600;
+
+
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientWindow();
+            }
+        });
+    }
+
+    private final JTextArea log = new JTextArea();
+    private final JTextField fieldNickname = new JTextField("User");
+    private final JTextField fieldInput = new JTextField();
+
+
+    private ClientWindow(){
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(WIDTH,HEIGHT);
+        setLocationRelativeTo(null);
+        setAlwaysOnTop(true);
+        log.setEditable(false);
+        log.setLineWrap(true);
+        add(log, BorderLayout.CENTER);
+        add(fieldInput, BorderLayout.SOUTH);
+        add(fieldNickname, BorderLayout.NORTH);
+        setVisible(true);
 
     }
 }
